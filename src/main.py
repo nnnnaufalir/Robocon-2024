1e8bd310dda59aa3224333e28fae7364546d6dd9
Naufal Ilham Ramadhan <111553406+nnnnaufalir@users.noreply.github.com>
2024-06-01 08:23:30 +0700

    update com

--- src/main.py
+++ src/main.py
@@ -7,6 +7,14 @@ from lib_krai.krai_serial import SerialCommunicator as ser
 
 print("Import Success")
 
+# Initialize Serial Communication
+serial_0 = ser("COM12", 57600)
+serial_0.begin()
+time.sleep(1)
+
+serial_0.flush_input()
+serial_0.flush_output()
+
 
 def load_model(path):
     """Load the YOLO model."""
@@ -15,7 +23,7 @@ def load_model(path):
     return model
 
 
-def draw_screen(frame, frame_width, frame_height, fps, error, device_value):
+def draw_screen(frame, frame_width, frame_height, fps, angle, device_value, data_sent):
     """Draw informational elements on the screen."""
     center_line = frame_width // 2
     color_line = (0, 255, 0)
@@ -29,26 +37,23 @@ def draw_screen(frame, frame_width, frame_height, fps, error, device_value):
                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, color_line, 2)
     cv2.putText(frame, f'FPS: {fps:.2f}', (10, 15),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, color_line, 2)
-    cv2.putText(frame, f'Position: {error}', (10, 35),
+    cv2.putText(frame, f'Position: {angle}', (10, 35),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, color_line, 2)
+    cv2.putText(frame, f'Sent: {data_sent}', (10, 55),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
 
 def main():
     model_path = "../models/yolov8_4.pt"
-
     class_names = ['B', 'BB', 'BBB', 'blue_ball', 'purple_ball', 'silo']
-
     class_colors = {
-        0: (100, 100, 100),      # B - silo 1
-        1: (50, 50, 50),    # BB - silo 2
-        2: (200, 200, 200),  # BBB - silo 3
-        3: (255, 0, 0),      # Blue Ball -
-        4: (255, 0, 255),    # Purple Ball - Magenta
-        5: (255, 255, 255),  # silo - White
-
+        0: (100, 100, 100),    # B - silo 1
+        1: (50, 50, 50),       # BB - silo 2
+        2: (200, 200, 200),    # BBB - silo 3
+        3: (255, 0, 0),        # Blue Ball
+        4: (255, 0, 255),      # Purple Ball - Magenta
+        5: (255, 255, 255)     # silo - White
     }
-
-    global device_value
 
     frame_width = 640
     frame_height = 640
@@ -58,169 +63,140 @@ def main():
 
     device_value = 'cpu'
     conf_value = 0.2
-    iou_value = 0.7
-    max_det_value = 5
+    iou_value = 0.5
+    max_det_value = 3
 
     model = load_model(model_path)
-    # cap1 = vd("../videos/Testing/Berantakan.mp4")  # Kamera pertama
-    # cap2 = vd("../videos/Testing/Test3.mp4")       # Kamera kedua
-    cap1 = vd(0)  # Kamera pertama
-    cap2 = vd(2)
+    cap1 = vd(0)  # First camera
 
     prev_time = time.time()
-    error = "Not Defined"
-    valid_class = False
+    angle = "Not Defined"
+    data_sent = "None"
 
-    while True:
-        frame1 = cap1.read_frame()
-        frame2 = cap2.read_frame()
-        if frame1 is None or frame2 is None:
-            break
+    try:
+        while True:
+            frame1 = cap1.read_frame()
+            if frame1 is None:
+                break
 
-        frame1 = cv2.resize(frame1, (frame_width, frame_height))
-        frame2 = cv2.resize(frame2, (frame_width//2, frame_height//2))
+            frame1 = cv2.resize(frame1, (frame_width, frame_height))
 
-        # Kamera pertama untuk mencari target
-        results1 = model.track(
-            source=[frame1],
-            conf=conf_value,
-            iou=iou_value,
-            imgsz=640,
-            half=False,
-            device=device_value,
-            max_det=max_det_value,
-            vid_stride=1,
-            stream_buffer=False,
-            visualize=False,
-            augment=False,
-            agnostic_nms=False,
-            classes=None,
-            retina_masks=False
-        )
+            results1 = model.track(
+                source=[frame1],
+                conf=conf_value,
+                iou=iou_value,
+                imgsz=640,
+                half=False,
+                device=device_value,
+                max_det=max_det_value,
+                vid_stride=1,
+                stream_buffer=False,
+                visualize=False,
+                augment=False,
+                agnostic_nms=False,
+                classes=None,
+                retina_masks=False
+            )
 
-        closest_distance = float('inf')
-        closest_ball_details = None
+            closest_distance = float('inf')
+            closest_ball_details = None
 
-        for result in results1:
-            boxes = result.boxes.xyxy
-            confidences = result.boxes.conf
-            class_ids = result.boxes.cls
-            track_ids = result.boxes.id if hasattr(
-                result.boxes, 'id') else None
+            for result in results1:
+                boxes = result.boxes.xyxy
+                confidences = result.boxes.conf
+                class_ids = result.boxes.cls
+                track_ids = result.boxes.id if hasattr(
+                    result.boxes, 'id') else None
 
-            if boxes is not None and confidences is not None and class_ids is not None and track_ids is not None:
-                for box, conf, cls_id, track_id in zip(boxes, confidences, class_ids, track_ids):
-                    if cls_id == 3:  # Blue Ball
+                if boxes is not None and confidences is not None and class_ids is not None and track_ids is not None:
+                    for box, conf, cls_id, track_id in zip(boxes, confidences, class_ids, track_ids):
+                        if cls_id == 3:  # Blue Ball
+                            x1, y1, x2, y2 = map(int, box)
+                            width_obj_pixel = det.calculate_obj_pixel(x1, x2)
+                            distance = det.calculate_distance(
+                                size_obj_cm, focal_length, width_obj_pixel)
+
+                            if distance < closest_distance:
+                                closest_distance = distance
+                                closest_ball_details = (
+                                    box, conf, cls_id, track_id, distance)
+
+            for result in results1:
+                boxes = result.boxes.xyxy
+                confidences = result.boxes.conf
+                class_ids = result.boxes.cls
+                track_ids = result.boxes.id if hasattr(
+                    result.boxes, 'id') else None
+
+                if boxes is not None and confidences is not None and class_ids is not None and track_ids is not None:
+                    for box, conf, cls_id, track_id in zip(boxes, confidences, class_ids, track_ids):
                         x1, y1, x2, y2 = map(int, box)
-                        width_obj_pixel = det.calculate_obj_pixel(x1, x2)
-                        distance = det.calculate_distance(
-                            size_obj_cm, focal_length, width_obj_pixel)
+                        color = class_colors.get(int(cls_id), (0, 0, 0))
 
-                        if distance < closest_distance:
-                            closest_distance = distance
-                            closest_ball_details = (
-                                box, conf, cls_id, track_id, distance)
-
-        for result in results1:
-            boxes = result.boxes.xyxy
-            confidences = result.boxes.conf
-            class_ids = result.boxes.cls
-            track_ids = result.boxes.id if hasattr(
-                result.boxes, 'id') else None
-
-            if boxes is not None and confidences is not None and class_ids is not None and track_ids is not None:
-                for box, conf, cls_id, track_id in zip(boxes, confidences, class_ids, track_ids):
-                    x1, y1, x2, y2 = map(int, box)
-                    color = class_colors.get(int(cls_id), (0, 0, 0))
-
-                    # Jika deteksi adalah target atau tidak, tampilkan bounding box
-                    class_name = class_names[int(cls_id)]
-                    label = f'{class_name}: {conf:.2f} ID: {int(track_id)}'
-                    cv2.rectangle(frame1, (x1, y1), (x2, y2), color, 2)
-                    cv2.putText(frame1, label, (x1, y1 - 10),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-                    if closest_ball_details and torch.equal(box, closest_ball_details[0]):
-                        # Red color for the closest blue ball
-                        color = (0, 0, 255)
                         class_name = class_names[int(cls_id)]
                         label = f'{class_name}: {conf:.2f} ID: {int(track_id)}'
                         cv2.rectangle(frame1, (x1, y1), (x2, y2), color, 2)
                         cv2.putText(frame1, label, (x1, y1 - 10),
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-                        center_x, center_y = det.draw_point(
-                            frame1, frame_width, frame_height, x1, y1, x2, y2, color)
-                        error = det.error_pixel(center_x, center_y)
-                        distance_display = f'{closest_ball_details[4]:.2f} cm'
-                        cv2.putText(frame1, distance_display, (center_x + 10,
-                                    center_y + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-                        bottom_center_x = frame_width // 2
-                        bottom_center_y = frame_height
 
-                        cv2.line(frame1, (center_x, center_y),
-                                 (bottom_center_x, bottom_center_y), (255, 255, 255), 2)
-                    else:
-                        error = "Not Defined"
+                        if closest_ball_details and torch.equal(box, closest_ball_details[0]):
+                            color = (0, 0, 255)
+                            cv2.rectangle(frame1, (x1, y1), (x2, y2), color, 2)
+                            cv2.putText(frame1, label, (x1, y1 - 10),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                            center_x, center_y = det.draw_point(
+                                frame1, frame_width, frame_height, x1, y1, x2, y2, color)
+                            angle = det.calculate_angle(
+                                center_x, center_y, frame_width, frame_height)
 
-        # Kamera kedua untuk memvalidasi warna bola yang diambil oleh robot
-        results2 = model.track(
-            source=[frame2],
-            conf=conf_value,
-            iou=iou_value,
-            imgsz=320,
-            half=False,
-            device=device_value,
-            max_det=1,
-            vid_stride=10,
-            stream_buffer=False,
-            visualize=False,
-            augment=False,
-            agnostic_nms=False,
-            classes=[3, 4],  # Only detect blue_ball (0) and purple_ball (1)
-            retina_masks=False
-        )
+                            distance_display = f'{closest_ball_details[4]:.2f} cm'
+                            cv2.putText(frame1, distance_display, (center_x + 10,
+                                        center_y + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                            bottom_center_x = frame_width // 2
+                            bottom_center_y = frame_height
 
-        valid_class = False  # Reset valid_class for each frame
-        for result in results2:
-            boxes = result.boxes.xyxy
-            confidences = result.boxes.conf
-            class_ids = result.boxes.cls
-            track_ids = result.boxes.id if hasattr(
-                result.boxes, 'id') else None
+                            cv2.line(frame1, (center_x, center_y),
+                                     (bottom_center_x, bottom_center_y), (255, 255, 255), 2)
 
-            if boxes is not None and confidences is not None and class_ids is not None and track_ids is not None:
-                for box, conf, cls_id, track_id in zip(boxes, confidences, class_ids, track_ids):
-                    x1, y1, x2, y2 = map(int, box)
-                    color = class_colors.get(int(cls_id), (0, 0, 0))
+            fps, prev_time = det.calculate_fps(prev_time)
 
-                    # Validasi bola yang diambil oleh robot
-                    if cls_id == 0:  # Blue Ball
-                        valid_class = True
-                    else:
-                        valid_class = False
+            if closest_ball_details:
+                # Ensure angle is a float and format it
+                angle_display = f'{float(angle):.2f}'
 
-                    class_name = class_names[int(cls_id)]
-                    label = f'{class_name}: {conf:.2f} ID: {int(track_id)}'
-                    cv2.rectangle(frame2, (x1, y1), (x2, y2), color, 2)
-                    cv2.putText(frame2, label, (x1, y1 - 10),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                # Determine the command based on the angle
+                # If the angle is within 2 degrees of 0 or 180
+                if abs(float(angle)) <= 7.5 or abs(float(angle)) >= 172.5:
+                    command = "maju"
 
-        fps, prev_time = det.calculate_fps(prev_time)
+                else:
+                    command = "rotasi"
 
-        draw_screen(frame1, frame_width, frame_height,
-                    fps, error, device_value)
-        draw_screen(frame2, frame_width, frame_height,
-                    fps, valid_class, device_value)
+                # Prepare the data to be sent
+                distance = closest_ball_details[4]
+                data_sent = f"{command},{angle_display},{distance:.2f}"
+                serial_0.write(data_sent, 0)
+                print(f"Sent: {data_sent}")
 
-        cv2.imshow('Tracking', frame1)
-        cv2.imshow('Validator', frame2)
+            else:
+                angle_display = "Not Defined"
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Update the frame with the latest data_sent value
+            draw_screen(frame1, frame_width, frame_height,
+                        fps, angle_display, device_value, data_sent)
 
-    cap1.release()
-    cap2.release()
-    cv2.destroyAllWindows()
+            cv2.imshow('Tracking', frame1)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Close serial communication
+        serial_0.end()
+        cap1.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
